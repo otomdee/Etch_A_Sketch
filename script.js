bce34b2f3375a7287741b318247e21d573239bd9
otomdee <otomdaniel65@gmail.com>
2024-07-10 11:04:10 +0100

    add full grid

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let gridsize = 16;
 const container = document.querySelector("#container");
 
 for (let i = 0; i < gridsize; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cells");
-    container.append(cell);
+    const cellrow = document.createElement("div");
+    cellrow.classList.add("cellrow");
+    for (let j = 0; j <gridsize; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cells");
+        cellrow.append(cell);
+    }
+    container.append(cellrow);
 }
